0358f654014a68db03ca4bda90f4565030cb7533
Christopher_Madrid <Chris_Madrid@baylor.edu>
2018-03-09 16:35:11 +0100

    Now working with multiple data sets

--- Tools/test/fakeRatePlots.C
+++ Tools/test/fakeRatePlots.C
@@ -10,6 +10,10 @@
 #include <iostream>
 #include <string>
 
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
 class FakeRatePlots
 {
 
@@ -21,8 +25,10 @@ public:
     
     TH1F* defineTH1F(const char *file="", std::string branch="")
     {
-        TFile *f = new TFile(file);
+        //TFile *f = new TFile(file);
+        TFile *f = TFile::Open(file);
         TH1F *h = (TH1F*)f->Get(branch.c_str());
+        f->Close();
         return h;
     }
 
@@ -37,7 +43,7 @@ public:
         return error;
     }
   
-    void ratioTH1F(std::string fakerateVar, std::string Var, const char* file, std::string fakerate)
+    void ratioTH1F(std::string fakerateVar, std::string Var, const char* file, std::string fakerate, std::string type, std::string xname, std::string yname)
     {
         TH1F* fakeRateVar = defineTH1F(file,fakerateVar);
         TH1F* var         = defineTH1F(file,Var);
@@ -50,25 +56,27 @@ public:
             fakeRate->SetBinContent(i,ratio);
             fakeRate->SetBinError(i,error);
         }
-        fakeRate->SetTitle(fakerate.c_str());
+        fakeRate->SetTitle((type + "/" + fakerate).c_str());
+        fakeRate->GetXaxis()->SetTitle(xname.c_str());
+        fakeRate->GetYaxis()->SetTitle(yname.c_str());
         histos_.emplace_back(fakeRate);
-        histoName_.emplace_back(fakerate);
+        histoName_.emplace_back(type + "/" + fakerate);
     }
 
-    void makeTH1F(std::string name, const char* rootFile)
+    void makeTH1F(std::string name, const char* rootFile, std::string type)
     {
-        ratioTH1F( name + "fakerateMET"     ,name + "MET"              ,rootFile, name + "fakerateMET"              );
-        ratioTH1F( name + "fakerateNj"      ,name + "nJets"            ,rootFile, name + "fakerateNjets"            );
-        ratioTH1F( name + "fakerateNb"      ,name + "nBJets"           ,rootFile, name + "fakerateNbjets"           );			     
-        ratioTH1F( name + "fakerateMET2"    ,name + "MET"              ,rootFile, name + "fakerateMET2"             );
-        ratioTH1F( name + "fakerateNj2"     ,name + "nJets"            ,rootFile, name + "fakerateNjets2"           );
-        ratioTH1F( name + "fakerateNb2"     ,name + "nBJets"           ,rootFile, name + "fakerateNbjets2"          );    			     
-        ratioTH1F( name + "randomTopPt"     ,name + "randomTopCandPt"  ,rootFile, name + "fakerateRandomTopPt"      );
-        ratioTH1F( name + "randomTopMass"   ,name + "randomTopCandMass",rootFile, name + "fakerateRandomTopMass"    );
-        ratioTH1F( name + "randomTopEta"    ,name + "randomTopCandEta" ,rootFile, name + "fakerateRandomTopEta"     );    
-        ratioTH1F( name + "genTopMatchPt"   ,name + "genTopPt"         ,rootFile, name + "efficiencyGenTopMatchPt"  );
-        ratioTH1F( name + "genTopMatchMass" ,name + "genTopMass"       ,rootFile, name + "efficiencyGenTopMatchMass");	
-        ratioTH1F( name + "genTopMatchEta"  ,name + "genTopEta"        ,rootFile, name + "efficiencyGenTopMatchEta" );
+        ratioTH1F( name + "fakerateMET"     ,name + "MET"               ,rootFile ,name + "fakerateMET"              ,type ,"MET (GeV)"     ,"Fakerate"  );
+        ratioTH1F( name + "fakerateNj"      ,name + "nJets"             ,rootFile ,name + "fakerateNjets"            ,type ,"N_{Jets}"      ,"Fakerate"  );
+        ratioTH1F( name + "fakerateNb"      ,name + "nBJets"            ,rootFile ,name + "fakerateNbjets"           ,type ,"N_{BJets}"     ,"Fakerate"  );			     
+        ratioTH1F( name + "fakerateMET2"    ,name + "MET"               ,rootFile ,name + "fakerateMET2"             ,type ,"MET (GeV)"     ,"Fakerate"  );
+        ratioTH1F( name + "fakerateNj2"     ,name + "nJets"             ,rootFile ,name + "fakerateNjets2"           ,type ,"N Jets"        ,"Fakerate"  );
+        ratioTH1F( name + "fakerateNb2"     ,name + "nBJets"            ,rootFile ,name + "fakerateNbjets2"          ,type ,"N BJets"       ,"Fakerate"  );    			     
+        ratioTH1F( name + "randomTopPt"     ,name + "randomTopCandPt"   ,rootFile ,name + "fakerateRandomTopPt"      ,type ,"P_{T} (GeV)"   ,"Fakerate"  );
+        ratioTH1F( name + "randomTopMass"   ,name + "randomTopCandMass" ,rootFile ,name + "fakerateRandomTopMass"    ,type ,"Mass (GeV)"    ,"Fakerate"  );
+        ratioTH1F( name + "randomTopEta"    ,name + "randomTopCandEta"  ,rootFile ,name + "fakerateRandomTopEta"     ,type ,"#eta"          ,"Fakerate"  );    
+        ratioTH1F( name + "genTopMatchPt"   ,name + "genTopPt"          ,rootFile ,name + "efficiencyGenTopMatchPt"  ,type ,"P_{T} (GeV)"   ,"Efficiency");
+        ratioTH1F( name + "genTopMatchMass" ,name + "genTopMass"        ,rootFile ,name + "efficiencyGenTopMatchMass",type ,"Mass (GeV)"    ,"Efficiency");	
+        ratioTH1F( name + "genTopMatchEta"  ,name + "genTopEta"         ,rootFile ,name + "efficiencyGenTopMatchEta" ,type ,"#eta"          ,"Efficiency");
     }
     
     FakeRatePlots(){}
@@ -86,15 +94,13 @@ void makePlots(std::string name, TH1* simpleHist, TH1* mediumHist)
     gPad->SetRightMargin(0.05);
     gPad->SetLeftMargin(0.16);
     
-    //mediumHist->GetXaxis()->SetTitle("time (ns)");
-    //mediumHist->GetYaxis()->SetTitle("A.U.");
-    //mediumHist->SetTitle("Pulse Shape Comparison");
-    //mediumHist->SetName("Pulse Shape  Comparison");
+    mediumHist->SetMaximum( 1.2*( mediumHist->GetMaximum() ) );
+    mediumHist->SetMinimum(0);
     mediumHist->SetTitleSize(0.002);
     mediumHist->SetTitleSize(0.05,"X");
-    mediumHist->SetTitleSize(0.04,"Y");
+    mediumHist->SetTitleSize(0.05,"Y");
     mediumHist->SetTitleOffset(1.2,"X");
-    mediumHist->SetTitleOffset(1.8,"Y");
+    mediumHist->SetTitleOffset(1.5,"Y");
     mediumHist->SetLabelSize(0.05,"X");
     mediumHist->SetLabelSize(0.05,"Y");
     mediumHist->SetStats(false);    
@@ -114,17 +120,24 @@ void makePlots(std::string name, TH1* simpleHist, TH1* mediumHist)
     c->SaveAs(("plots/" + name + ".png").c_str());        
 }
 
-int main()
-{    
-    ///////////////////////////////////
-    //Simple Top Tagger
-    ///////////////////////////////////  
-    const char* rootFileSimple = "joesGroup/TT_TTbarSingleLep_simpleTopTagger.root";
-    std::string filenameSimple = "efficiencyandFakeRatePlots_TT_TTbarSingleLep_simpleTopTagger.root";
+void runPlotter(const char* rootFileSimple, std::string filenameSimple, const char* rootFileMedium, std::string filenameMedium)
+{
+
+    char copy[128];
+    strcpy(copy, rootFileSimple);    
+    char* type1;
+    type1 = strtok( copy, "-/" );
+    char* type2;
+    type2 = strtok( nullptr, "-/" );
+    char* type3;
+    type3 = strtok( nullptr, "-/" );
     
-    FakeRatePlots fakeratePlotsSimple;    
-    fakeratePlotsSimple.makeTH1F("Lep0_", rootFileSimple);
-    fakeratePlotsSimple.makeTH1F("Lep1_", rootFileSimple);
+    ///////////////////////////////
+    //      Simple TopTagger
+    ///////////////////////////////
+    FakeRatePlots fakeratePlotsSimple;
+    fakeratePlotsSimple.makeTH1F("Lep0/", rootFileSimple, type3);
+    fakeratePlotsSimple.makeTH1F("Lep1/", rootFileSimple, type3);
 
     TFile *fSimple = new TFile(filenameSimple.c_str(),"RECREATE");
     if(fSimple->IsZombie())
@@ -132,49 +145,63 @@ int main()
         std::cout << "Cannot create " << filenameSimple << std::endl;
         throw "File is zombie";
     }
-
+    
     fSimple->cd();
-
+    
     std::cout<<"Filling Simple Top Tagger Histo ;) "<<std::endl;
     for(int i = 0; i < fakeratePlotsSimple.histos_.size(); i++)
     {
         fakeratePlotsSimple.histos_[i]->Write();
     }
-
+    
     fSimple->Write();
     fSimple->Close();      
-
-    ///////////////////////////////////
-    //Medium Top Tagger
-    ///////////////////////////////////  
-
-    const char* rootFileMedium = "joesGroup/TT_TTbarSingleLep_mediumTopTagger.root";    
-    std::string filenameMedium = "efficiencyandFakeRatePlots_TT_TTbarSingleLep_mediumTopTagger.root";
-
-    FakeRatePlots fakeratePlotsMedium;    
-    fakeratePlotsMedium.makeTH1F("Lep0_", rootFileMedium);
-    fakeratePlotsMedium.makeTH1F("Lep1_", rootFileMedium);
-
+    
+    ///////////////////////////////
+    //      Medium TopTagger
+    ///////////////////////////////    
+    FakeRatePlots fakeratePlotsMedium;
+    fakeratePlotsMedium.makeTH1F("Lep0/", rootFileMedium, type3);
+    fakeratePlotsMedium.makeTH1F("Lep1/", rootFileMedium, type3);
+    
     TFile *fMedium = new TFile(filenameMedium.c_str(),"RECREATE");
     if(fMedium->IsZombie())
     {
         std::cout << "Cannot create " << filenameMedium << std::endl;
         throw "File is zombie";
     }
-
+    
     fMedium->cd();
-
+    
     std::cout<<"Filling Medium Top Tagger Histo ;) "<<std::endl;
     for(int i = 0; i < fakeratePlotsMedium.histos_.size(); i++)
     {
         fakeratePlotsMedium.histos_[i]->Write();
     }
-
+    
     fMedium->Write();
-    fMedium->Close();
-
+    fMedium->Close();      
+    
+    ////////////////////////////////
+    //       Making Plots
+    ////////////////////////////////
     for(int i = 0; i < fakeratePlotsSimple.histos_.size(); i++)
     {
         makePlots(fakeratePlotsSimple.histoName_[i], fakeratePlotsSimple.histos_[i], fakeratePlotsMedium.histos_[i]);
     }
+}
+
+int main()
+{
+    TH1::AddDirectory(false);
+
+    runPlotter("joesGroup/SimpleHaddFiles/TT_TTbar-2018-3-9.root", "efficiencyandFakeRatePlots_TT_TTbar_simpleTopTagger.root",
+               "joesGroup/MediumHaddFiles/TT_TTbar-2018-3-9.root", "efficiencyandFakeRatePlots_TT_TTbar_mediumTopTagger.root"
+              );
+    runPlotter("joesGroup/SimpleHaddFiles/TT_QCD-2018-3-9.root", "efficiencyandFakeRatePlots_TT_QCD_simpleTopTagger.root",
+               "joesGroup/MediumHaddFiles/TT_QCD-2018-3-9.root", "efficiencyandFakeRatePlots_TT_QCD_mediumTopTagger.root"
+              );
+    runPlotter("joesGroup/SimpleHaddFiles/TT_TTbarSingleLep-2018-3-9.root", "efficiencyandFakeRatePlots_TT_TTbarSingleLep_simpleTopTagger.root",
+               "joesGroup/MediumHaddFiles/TT_TTbarSingleLep-2018-3-9.root", "efficiencyandFakeRatePlots_TT_TTbarSingleLep_mediumTopTagger.root"
+              );
 }
