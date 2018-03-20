a0fc76a76281967582fd8c6651937c9b75010fda
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-03-20 17:57:52 -0500

    Starting to make histocontainer work for stealth group

--- Tools/include/HistoContainer.h
+++ Tools/include/HistoContainer.h
@@ -166,26 +166,56 @@ public:
 
     void setVar(const TUPLECLASS& tr)
     {
-        met_                 = &tr.template getVar<double>(           "met");
-        metphi_              = &tr.template getVar<double>(           "metphi");    
-        ht_                  = &tr.template getVar<double>(           "HT");
-        vtxSize_             = &tr.template getVar<int>(              "vtxSize");
-        cntCSVS_             = &tr.template getVar<int>(              "cntCSVS");
-        ttr_                 =  tr.template getVar<TopTaggerResults*>("ttrMVA");    
-        //cutMuVec_            = &tr.template getVec<TLorentzVector>(   "cutMuVec");
-        //cutElecVec_          = &tr.template getVec<TLorentzVector>(   "cutElecVec");    
-        cntNJetsPt30Eta24_   = &tr.template getVar<int>(              "cntNJetsPt30Eta24");
-        lepton_              = &tr.template getVar<TLorentzVector>(   "lepton");
-        genTops_             = &tr.template getVec<TLorentzVector>(   "genTops");
-        bestCandLV_          = &tr.template getVar<TLorentzVector>(   "bestTopMassLV");
-        bestTopMass_         = &tr.template getVar<double>(           "bestTopMass");
-        bestTopMassTopTag_   = &tr.template getVar<bool>(             "bestTopMassTopTag");
-        bestTopMassGenMatch_ = &tr.template getVar<bool>(             "bestTopMassGenMatch");
+        if( &tr.template getVar<double>("met") )
+        {
+            met_                 = &tr.template getVar<double>(           "met");
+            metphi_              = &tr.template getVar<double>(           "metphi");    
+            ht_                  = &tr.template getVar<double>(           "HT");
+            vtxSize_             = &tr.template getVar<int>(              "vtxSize");
+            cntCSVS_             = &tr.template getVar<int>(              "cntCSVS");
+            ttr_                 =  tr.template getVar<TopTaggerResults*>("ttrMVA");    
+            cntNJetsPt30Eta24_   = &tr.template getVar<int>(              "cntNJetsPt30Eta24");
+            lepton_              = &tr.template getVar<TLorentzVector>(   "lepton");
+            genTops_             = &tr.template getVec<TLorentzVector>(   "genTops");
+            bestCandLV_          = &tr.template getVar<TLorentzVector>(   "bestTopMassLV");
+            bestTopMass_         = &tr.template getVar<double>(           "bestTopMass");
+            bestTopMassTopTag_   = &tr.template getVar<bool>(             "bestTopMassTopTag");
+            bestTopMassGenMatch_ = &tr.template getVar<bool>(             "bestTopMassGenMatch");
+        }
+        else if(*tr.MET)
+        {
+            met_                 = *tr.MET;
+            metphi_              = *tr.METPhi;
+            ht_                  = *tr.HT;
+            vtxSize_             = *tr.NVtx;
+            //cntCSVS_             = *tr.;
+            //ttr_                 = *tr.;
+            //cntNJetsPt30Eta24_   = *tr.;
+            //lepton_              = *tr.;
+            //genTops_             = *tr.;
+            //bestCandLV_          = *tr.;
+            //bestTopMass_         = *tr.;
+            //bestTopMassTopTag_   = *tr.;
+            //bestTopMassGenMatch_ = *tr.;
+        }
+        else
+        {
+            std::cout<<"In HistoContainer.h: No private variables"<<std::endl;
+        }
     }
 
+    ///Hack
+    ///Should be fix in simpleAnalyzer.C
     void fill(const TUPLECLASS& tr, const double& eWeight, TRandom* trand)
+    {
+        fill(tr, eWeight)
+    }
+
+    void fill(const TUPLECLASS& tr, const double& eWeight)
     {    
         setVar(tr);
+
+        TRandom* trand = new TRandom3();
 
         hMET->Fill(*met_, eWeight);
         hHT->Fill(*ht_, eWeight);
