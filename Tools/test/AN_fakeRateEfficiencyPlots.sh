5efd88268dc2ebde0bf947b9cc4ffb1dfb926115
Semra <Semra.Turkcapar@cern.ch>
2022-04-08 09:01:13 -0500

    this is the script to make fake rate and efficiency plots

---
+++ Tools/test/AN_fakeRateEfficiencyPlots.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# TT - efficiency 
+#./fakeRatePlots -y 2016preAPV -d TT -w 0.920 -x 0.950
+#./fakeRatePlots -y 2016preAPV -d TT -w 0.920 -x 0.960
+#./fakeRatePlots -y 2016preAPV -d TT -w 0.920 -x 0.970
+#./fakeRatePlots -y 2016preAPV -d TT -w 0.920 -x 0.980
+#./fakeRatePlots -y 2016preAPV -d TT -w 0.920 -x 0.990
+
+./fakeRatePlots -y 2016postAPV -d TT -w 0.920 -x 0.950
+#./fakeRatePlots -y 2016postAPV -d TT -w 0.920 -x 0.960
+#./fakeRatePlots -y 2016postAPV -d TT -w 0.920 -x 0.970
+#./fakeRatePlots -y 2016postAPV -d TT -w 0.920 -x 0.980
+#./fakeRatePlots -y 2016postAPV -d TT -w 0.920 -x 0.990
+
+#./fakeRatePlots -y 2017 -d TT -w 0.920 -x 0.950
+#./fakeRatePlots -y 2017 -d TT -w 0.920 -x 0.960
+#./fakeRatePlots -y 2017 -d TT -w 0.920 -x 0.970
+#./fakeRatePlots -y 2017 -d TT -w 0.920 -x 0.980
+#./fakeRatePlots -y 2017 -d TT -w 0.920 -x 0.990
+#
+#./fakeRatePlots -y 2018 -d TT -w 0.920 -x 0.950
+#./fakeRatePlots -y 2018 -d TT -w 0.920 -x 0.960
+#./fakeRatePlots -y 2018 -d TT -w 0.920 -x 0.970
+#./fakeRatePlots -y 2018 -d TT -w 0.920 -x 0.980
+#./fakeRatePlots -y 2018 -d TT -w 0.920 -x 0.990
+
+# QCD - fakeRate
+#./fakeRatePlots -y 2016preAPV -d QCD -w 0.920 -x 0.950
+#./fakeRatePlots -y 2016preAPV -d QCD -w 0.920 -x 0.960
+#./fakeRatePlots -y 2016preAPV -d QCD -w 0.920 -x 0.970
+#./fakeRatePlots -y 2016preAPV -d QCD -w 0.920 -x 0.980
+#./fakeRatePlots -y 2016preAPV -d QCD -w 0.920 -x 0.990
+
+./fakeRatePlots -y 2016postAPV -d QCD -w 0.920 -x 0.950
+#./fakeRatePlots -y 2016postAPV -d QCD -w 0.920 -x 0.960
+#./fakeRatePlots -y 2016postAPV -d QCD -w 0.920 -x 0.970
+#./fakeRatePlots -y 2016postAPV -d QCD -w 0.920 -x 0.980
+#./fakeRatePlots -y 2016postAPV -d QCD -w 0.920 -x 0.990
+
+#./fakeRatePlots -y 2017 -d QCD -w 0.920 -x 0.950
+#./fakeRatePlots -y 2017 -d QCD -w 0.920 -x 0.960
+#./fakeRatePlots -y 2017 -d QCD -w 0.920 -x 0.970
+#./fakeRatePlots -y 2017 -d QCD -w 0.920 -x 0.980
+#./fakeRatePlots -y 2017 -d QCD -w 0.920 -x 0.990
+#
+#./fakeRatePlots -y 2018 -d QCD -w 0.920 -x 0.950
+#./fakeRatePlots -y 2018 -d QCD -w 0.920 -x 0.960
+#./fakeRatePlots -y 2018 -d QCD -w 0.920 -x 0.970
+#./fakeRatePlots -y 2018 -d QCD -w 0.920 -x 0.980
+#./fakeRatePlots -y 2018 -d QCD -w 0.920 -x 0.990
